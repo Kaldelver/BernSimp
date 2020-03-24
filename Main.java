aeed05e8dc13e12e5cb934275bbf72c9d4697d47
hongsaizi <62575015+hongsaizi@users.noreply.github.com>
2020-03-24 00:11:33 -0400

    Movement with Keyboard

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 
 public class Main {
 	
@@ -6,14 +10,26 @@ public class Main {
 		objectbernie berns = new objectbernie();
 		
 		JFrame frame = new JFrame("Bernsimp");
+
+		//this will have to change somehow set it in project folder
+		ImageIcon ii = new ImageIcon("C:\\Users\\*********\\Desktop\\Yang_gang\\pixil-frame-0.png");
 		
-		//add the object Bernie into the frame
-		frame.add(berns);
-		
+		//calling ImageIcon
+	    JLabel lable = new JLabel(ii);
+	       
+	    @SuppressWarnings("unused")
+		JScrollPane jsp = new JScrollPane(lable);
+	       
+	    //for jsp variable is defined for later
+	    frame.getContentPane().add(jsp);
+	       
 	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    //What happens when u close
 	    
 	    frame.setSize(1920,1080);
+		
+		//add the object Bernie into the frame
+		frame.add(berns);
 	    
 	    //size of frame
 	    frame.setVisible(true);
--- objectbernie.java
+++ objectbernie.java
@@ -2,31 +2,41 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.geom.*;
 
 import javax.swing.*;;
 
-	public class objectbernie extends JPanel implements ActionListener{
+	public class objectbernie extends JPanel implements ActionListener, KeyListener{
 		
 		//creates the timer
 		Timer t = new Timer(5, this);
 		
 		//variables for the position and velocity of each direction
-		double xPos =  0, yPos = 0, velX = 2, velY = 2;
+		double xPos =  0, yPos = 0, xVel = 0, yVel = 0;
+		
+		//makes it so that it keeps going a direction even when a key is released
+		boolean checkUp = false, checkDown = false, checkRight = false, checkLeft = false;
+		
+		//sets up the key detection for the class
+		public objectbernie(){
+			t.start();
+			addKeyListener(this);
+			setFocusable(true);
+			setFocusTraversalKeysEnabled(false);
+		}
 	
 		public void paintComponent(Graphics g){
 			
 			super.paintComponent(g);
 			Graphics2D g2 = (Graphics2D) g;
 			
-			//create new object with positions and sizes
-			Ellipse2D circle = new Ellipse2D.Double(xPos, yPos, 40, 40);
+			//create character object with positions and sizes
+			Rectangle2D rectangle = new Rectangle2D.Double(xPos, yPos, 40, 40);
 			
-			//fill the object
-			g2.fill(circle);
-			
-			//start the timer
-			t.start();
+			//fill the object (later replaced with sprite)
+			g2.fill(rectangle);
 			
 		}
 
@@ -34,12 +44,99 @@ import javax.swing.*;;
 		public void actionPerformed(ActionEvent e) {
 			
 			//move the object by the velocity
-			xPos += velX;
-			yPos += velY;
+			xPos += xVel;
+			yPos += yVel;
 			
 			//refreshes the screen
-			repaint();
+			repaint();	
+		}
+
+		//changes the velocity based on which direction is pressed
+		public void up() {
+			yVel = -1.5;
+		}
+		
+		public void down() {
+			yVel = 1.5;
+		}
+	
+		public void left() {
+			xVel = -1.5;
+		}
+		
+		public void right() {
+			xVel = 1.5;
+		}
+		
+		public void rest() {
 			
+			if (checkLeft == false && checkRight == false) {
+				xVel = 0;
+			}
+			
+			if (checkUp == false && checkDown == false) {
+				
+				yVel = 0;
+			}
+		}
+		
+		//detects the key presses
+		public void keyPressed(KeyEvent e) {
+			
+			//since each key has a unique code, store it into a variable
+			int code = e.getKeyCode();
+			
+			//check if the code matches with each direction and call the class
+			if (code == KeyEvent.VK_UP) {
+				up();
+				checkUp = true;
+			}
+			
+			if (code == KeyEvent.VK_DOWN) {
+				down();
+				checkDown = true;
+			}
+			
+			if (code == KeyEvent.VK_LEFT) {
+				left();
+				checkLeft = true;
+			}
+			
+			if (code == KeyEvent.VK_RIGHT) {
+				right();
+				checkRight = true;
+			}
+		}
+		
+		@Override
+		public void keyTyped(KeyEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void keyReleased(KeyEvent e) {
+			int code = e.getKeyCode();
+			
+			if (code == KeyEvent.VK_UP) {
+				checkUp = false;
+				rest();
+			}
+			
+			if (code == KeyEvent.VK_DOWN) {
+				checkDown = false;
+				rest();
+			}
+			
+			if (code == KeyEvent.VK_LEFT) {
+				checkLeft = false;
+				rest();
+			}
+			
+			if (code == KeyEvent.VK_RIGHT) {
+				checkRight = false;
+				rest();
+			}
 		}
 
 
